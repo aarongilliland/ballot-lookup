324c114c5b4ceda8ec800dcc2dfcac384a256b56
Aaron <aarongilliland@gmail.com>
2024-09-11 20:33:19 -0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter,belleza } from '@/app/ui/fonts';
+import { belleza } from '@/app/ui/fonts';
  
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-600 p-4 md:h-52">
         <SarpyLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-600 px-6 py-10 md:w-2/5 md:px-20">
         <p className={`${belleza.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Ballot Lookup</strong> ... find your sample ballot{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://sarpygop.com" className="text-blue-500">
               Search by your Street Number and Street Name
             </a>
             , brought to you by Sarpy County GOP.
